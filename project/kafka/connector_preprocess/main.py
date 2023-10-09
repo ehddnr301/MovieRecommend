a4248212e73bf56a4322ee68406fd4d15a527940
ehddnr301 <dy950328@gmail.com>
2023-10-09 04:47:13 +0000

    Add more connector

--- project/kafka/connector_preprocess/main.py
+++ project/kafka/connector_preprocess/main.py
@@ -1,33 +1,29 @@
 import subprocess
 import json
+import os
+
+
+def post_connector(json_file):
+    subprocess.run(
+        [
+            "curl",
+            "-X",
+            "POST",
+            "http://connect-service:8083/connectors",
+            "-H",
+            "Content-Type: application/json",
+            "-d",
+            f"@{json_file}",
+        ]
+    )
 
 
 def main():
-    subprocess.run(
-        [
-            "curl",
-            "-X",
-            "POST",
-            "http://connect-service:8083/connectors",
-            "-H",
-            "Content-Type: application/json",
-            "-d",
-            "@source_connector.json",
-        ]
-    )
-
-    subprocess.run(
-        [
-            "curl",
-            "-X",
-            "POST",
-            "http://connect-service:8083/connectors",
-            "-H",
-            "Content-Type: application/json",
-            "-d",
-            "@sink_connector.json",
-        ]
-    )
+    # 현재 디렉토리의 모든 파일을 나열합니다.
+    for filename in os.listdir("."):
+        # .json 확장자를 가진 파일만 처리합니다.
+        if filename.endswith(".json"):
+            post_connector(filename)
 
 
 if __name__ == "__main__":
